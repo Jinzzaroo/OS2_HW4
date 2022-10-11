6df691cc4eed711862a186483aca957be6e913c6
JinYeongKim <realzero101@naver.com>
2022-10-11 14:06:36 +0900

    Add files via upload

---
+++ Practice5_1/Practice1.java
@@ -0,0 +1,40 @@
+package Practice;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		ColorTV myTV = new ColorTV(32, 1024);
+		myTV.printProperty();
+
+	}
+
+}
+
+class TV {
+	private int size;
+
+	public TV(int size) {
+		this.size = size;
+	}
+
+	protected int getSize() {
+		return size;
+	}
+}
+
+class ColorTV extends TV {
+	private int numColor;
+
+	public ColorTV(int size, int numColor) {
+		super(size);
+		this.numColor = numColor;
+	}
+
+	public void printProperty() {
+		System.out.println(getSize() + "인치 " + numColor + "컬러");
+	}
+
+	public int getColor() {
+		return numColor;
+	}
+}
