4b65591cf2883dfb0adbfb0727be1505cfe79df9
JinYeongKim <realzero101@naver.com>
2022-10-11 14:01:09 +0900

    Add files via upload

---
+++ Practice/Practice7_2.java
@@ -0,0 +1,23 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Practice7_2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = null;
+		
+		while(true) {
+			System.out.print(">>");
+			s = sc.nextLine();
+			
+			if(s.equals("그만")) {
+				System.out.println("종료합니다...");
+				break;
+			}
+			
+			String a [] = s.split(" ");
+			System.out.println("어절 개수는 " + a.length);
+		}
+	}
+}
